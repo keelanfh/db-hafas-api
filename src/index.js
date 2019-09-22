b2f7c15a20534b49d0c0b0543f3fcd7c050a167a
Keelan Fadden-Hopper <info@keelanfh.com>
2019-09-22 14:01:54 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const createHafas = require("hafas-client");
+const dbProfile = require("hafas-client/p/db");
+
+const createApi = require("..");
+
+// pro tip: pipe this script into `pino-pretty` to get nice logs
+
+const config = {
+  hostname: process.env.HOSTNAME || "n0lc8.sse.codesandbox.io",
+  port: process.env.PORT || 80, // todo [breaking]: remove, not necessary
+  name: "db-rest",
+  description: "An HTTP API for Deutsche Bahn.",
+  homepage: "http://example.org/",
+  docsLink: "http://example.org/docs",
+  logging: true,
+  healthCheck: () => {
+    return hafas.stop("8011306").then(stop => !!stop);
+  }
+};
+
+const hafas = createHafas(dbProfile, "hafas-rest-api-example");
+
+const api = createApi(hafas, config, () => {});
+
+const { logger } = api.locals;
+api.listen(config.port, err => {
+  if (err) {
+    logger.error(err);
+    process.exitCode = 1;
+  } else {
+    logger.info(`Listening on ${config.hostname}:${config.port}.`);
+  }
+});
